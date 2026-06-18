8e69eba292cd7bd574803a4e596ba4cbb68ec2dc
nkoreli <nika.koreli@gmail.com>
2026-06-18 12:19:17 -0700

    latest-eddsa

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 export {default as buildBabyjub} from "./src/babyjub.js";
 
-
+export {default as buildEddsa} from "./src/eddsa.js";
 
 export { buildPoseidon, buildPoseidonWasm } from "./src/poseidon_wasm.js";
 
